a8bd63f59be567df7d6ef250df14dd3d4a393e09
Jean Paul Sierra <jeanpaulsierraboom@gmail.com>
2022-12-25 22:53:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,request
+from flask_restful import Resource,Api
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://jeanpaulsb:1041690395@pollingapp.yvnqz8y.mongodb.net/test')
+
+app = Flask(__name__)
+api = Api(app)
+
+
+
+class Register(Resource):
+    def post(self):
+        # getting our jurors db
+        db = client.Users
+        jurors = db.Jurors
+        
+        # handling data from html form
+        data = request.form
+
+        # user object
+        user = {
+            'email':data['email'],
+            'name':data['name'],
+            'upbId':data['upbId'],
+            'password':data['password'],
+            'participants':[]
+        }
+
+    
+
+        jurors.insert_one(user)
+
+
+
+        return 201
+
+api.add_resource(Register,"/api/register")
+
+if __name__ == '__main__':
+    app.run(debug=True)
